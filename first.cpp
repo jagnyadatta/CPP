0d79bca835e32c9ee35d1a1dd6fc098f33690778
jagnyadatta <jagnyadattadalai99215@gmail.com>
2024-02-22 18:16:03 +0530

    first cpp program

---
+++ first.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int i;
+    cout<<"Enter the value of I:\n";
+    cin>>i;
+    cout<<"Here is the value of I:\n";
+    cout<<i;
+    return 0;
+}
