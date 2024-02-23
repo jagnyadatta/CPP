6e01c0112c7a1b625dcc31c23fa4a70efde03aa0
jagnyadatta <jagnyadattadalai99215@gmail.com>
2024-02-23 22:52:55 +0530

    Question 4-vii

---
+++ Qn4-vii.cpp
@@ -0,0 +1,11 @@
+/* Find left shit, right shift and zero fill of summation of all digits of any
+4 digit number and it will be shifted by 3 rd digit of any 4 digit number? */
+#include<iostream>
+using namespace std;
+int main()
+{
+    
+}
+
+//NOTE
+//This Question is Doubt.. Plz Ask to Sir..
